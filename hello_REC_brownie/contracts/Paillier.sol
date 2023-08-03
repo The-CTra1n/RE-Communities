6af94fd7ccc6a79216b4da8c6be563f41669bcb0
Conor <conor.mcmenamin1994@gmail.com>
2023-08-03 11:03:41 +0200

    added paillier functions

--- hello_REC_brownie/contracts/Paillier.sol
+++ hello_REC_brownie/contracts/Paillier.sol
@@ -4,38 +4,27 @@ pragma solidity ^0.8.0;
 contract Paillier {
     uint256 public n;
     uint256 public g;
-    uint256 public lambda;
-    uint256 public mu;
 
-    constructor(uint256 _n, uint256 _g, uint256 _lambda, uint256 _mu) {
+    constructor(uint256 _n, uint256 _g) {
         n = _n;
         g = _g;
-        lambda = _lambda;
-        mu = _mu;
     }
 
     function encrypt(uint256 plaintext, uint256 randomness) external view returns (uint256) {
         require(plaintext < n, "Plaintext is too large");
         
-        uint256 randomness = generateRandom();
         uint256 ciphertext = (modPow(g, plaintext, n ** 2) * modPow(randomness, n, n ** 2)) % (n ** 2);
         
         return ciphertext;
     }
 
-    function decrypt(uint256 ciphertext) public view returns (uint256) {
+    function decrypt(uint256 ciphertext, uint256 lambda, uint256 mu) public view returns (uint256) {
         uint256 c = modPow(ciphertext, lambda, n ** 2);
         uint256 L = (c - 1) / n;
         
         return (L * mu) % n;
     }
 
-    function generateRandom() public view returns (uint256) {
-        // Generate a suitable random number for encryption
-        // This could involve using chain-specific randomness solutions
-        // For simplicity, we'll return a static value here
-        return 666;
-    }
 
     function modPow(uint256 base, uint256 exponent, uint256 modulus) internal pure returns (uint256) {
         // Modular exponentiation function
--- hello_REC_brownie/tests/test.py
+++ hello_REC_brownie/tests/test.py
@@ -1,21 +1,42 @@
 import pytest
 from brownie import Wei, reverts, chain, accounts
 
+from Crypto.Util import number
+import math
+from Utils.paillier import *
+
+
+
 def test_token_swap(accounts,Paillier, System):
     initial_supply = 100000000
-    p=13
-    q=17
-    n=p*q
-    g=4886
-    l=48
-    mu=159
+    n_length=16
+    n,g,l,mu=get_Paillier_params(n_length)
 
     auctioneer=accounts[0]
 
     accounts[0].transfer(accounts[1], "10 ether", gas_price=0)
-    # deploy the auction contract
-    enc_scheme=auctioneer.deploy(Paillier,n,g,l,mu)
+
+    
+
+    enc_scheme=auctioneer.deploy(Paillier,n,g)
+    
     re_system = auctioneer.deploy(System,auctioneer,enc_scheme)
 
     re_system.addPlayer(accounts[1], enc_scheme, {"from":accounts[1]})
-    print(re_system.num_people())
+
+
+def get_Paillier_params(n_length):
+    priv, pub = generate_keypair(n_length)
+    n=pub.n
+    g=pub.g
+    l=priv.l
+    mu=priv.m
+    return n,g,l,mu
+
+def paillier_encrypt(plaintext, randomness, n, g):
+    return ((pow(g,plaintext, n**2)*pow(randomness, n, n**2)))%(n**2)
+
+def paillier_decrypt(ciphertext, n, l, mu):
+    c=pow(ciphertext,l,n**2)
+    L=((c-1)/n)-((c-1)%n)
+    return((L*mu)%n)
